2c8c455dc783877c1727c5549568359aa9b12945
fncreative <dann.frey@me.com>
2018-07-11 13:20:01 -0700

    added lab-03

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-var nums = [1,2,3,4,5,6,7,8,9];
+'use strict';
 
-// for (start, stop, step) {};
+var userName, myAge, myState, myHobby, myCrash, mySiblings;
 
-for(var i = 0; i < nums.length; i++) {
-    console.log('index: ', i)
-    console.log('value: ', nums[i])
+userName = prompt('What is your name?');
+
+alert('These questions are answered with a Y or N.');
+
+myAge = prompt('Welcome ' + userName + '. Do you think that I am over 35?');
+console.log('am I over 35?', userName);
+if (myAge.toUpperCase() === 'Y') {
+  alert('You are correct, I am 39.');
+} else if (myAge.toUpperCase() === 'N') {
+  alert('Sorry, I actually am! I\'ll be 40 this year');
+} else {
+  alert('That was not the answer I wanted...Try Y or N next time.');
 }
-
-var multi = [[1,2,3], [4,5,6], [7,8,9,]]
-
-/* for (var i = 0; i < multi.length; i++) {
-    for (var j = 0; j < multi[i].length; j++)
-    console.log(multi[i][j])
-    // console.log(multi[i])
-} */
-
-var count = 0;
-
-while (count < 10) {
-    console.log('count', count);
-    count++;
+myState = prompt('Was I born in WA?');
+console.log('Born in WA?', myState);
+if (myState.toUpperCase() === 'Y') {
+  alert('Nope! I\'m from Illinois');
+} else if (myState.toUpperCase() === 'N') {
+  alert('You would be correct!');
+} else {
+  alert('That was not the answer I wanted...Try Y or N next time.');
 }
-
-
-/* do {
-    // do something
-} while(true) */
-
-
+myHobby = prompt('Do I like to hike?');
+console.log('Do I hike?', myHobby);
+if (myHobby.toUpperCase() === 'Y') {
+  alert('Aboslutely! Every weekend!');
+} else if (myHobby.toUpperCase() === 'N') {
+  alert('Sorry, I hate being stuck inside!');
+} else {
+  alert('That was not the answer I wanted...Try Y or N next time.');
+}
+myCrash = prompt('I used to fly for the military. Was I ever involved in a midair incident?');
+console.log('Plane Crash!!', myCrash);
+if (myCrash.toUpperCase() === 'Y') {
+  alert('You\'re right! We nearly died once.');
+} else if (myCrash.toUpperCase() === 'N') {
+  alert('Unfortunately you\'re wrong! We nearly crashed once.');
+} else {
+  alert('That was not the answer I wanted...Try Y or N next time.');
+}
+mySiblings = prompt('Do I have any siblings?');
+console.log('Do I have Siblings?', mySiblings);
+if (mySiblings.toUpperCase() === 'Y') {
+  alert('Right! I have two siblings who are both much older than me!');
+} else if (mySiblings.toUpperCase() === 'N') {
+  alert('I actually have two siblings! Both older!');
+} else {
+  alert('That was not the answer I wanted...Try Y or N next time.');
+}
