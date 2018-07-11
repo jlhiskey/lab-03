f39d94c26be7ab81049a3b084ec71a5828eebd8e
fncreative <dann.frey@me.com>
2018-07-11 13:00:21 -0700

    added lab-03

---
+++ app.js
@@ -0,0 +1,30 @@
+var nums = [1,2,3,4,5,6,7,8,9];
+
+// for (start, stop, step) {};
+
+for(var i = 0; i < nums.length; i++) {
+    console.log('index: ', i)
+    console.log('value: ', nums[i])
+}
+
+var multi = [[1,2,3], [4,5,6], [7,8,9,]]
+
+/* for (var i = 0; i < multi.length; i++) {
+    for (var j = 0; j < multi[i].length; j++)
+    console.log(multi[i][j])
+    // console.log(multi[i])
+} */
+
+var count = 0;
+
+while (count < 10) {
+    console.log('count', count);
+    count++;
+}
+
+
+/* do {
+    // do something
+} while(true) */
+
+
